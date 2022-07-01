a6abec83e2fe6b2dd965f69ab36d784f1f6c101b
JohnnyYours <103307019+JohnnyYours@users.noreply.github.com>
2022-07-01 16:59:09 +0300

    index.js, index.html and style.css added

---
+++ 1_getNavbarElement/js/index.js
@@ -0,0 +1,29 @@
+/*
+Завершите функции, чтобы они возвращали то, что ожидается. У каждого метода есть комментарий, который объясняет, что он должен вернуть.
+*/
+
+const getNavbarElement = () => {
+  // TODO: get the element with id navbar
+  return document.querySelector("#navbar");
+};
+
+const getMainElement = () => {
+  // TODO: get the element with id main
+  return document.querySelector("#main");
+};
+
+const getAboutFromFooter = () => {
+  // TODO: get the the about link that's in the footer
+  return document.querySelector("#footer-wrapper .about");
+};
+
+const getTheParagraphElement = () => {
+  // TODO: get the first paragraph element
+  return document.querySelector("p");
+};
+
+// Sample usage - do not modify
+console.log(getNavbarElement());
+console.log(getMainElement());
+console.log(getAboutFromFooter());
+console.log(getTheParagraphElement());
