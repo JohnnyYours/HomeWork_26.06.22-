a3a7ac354d3e50bb63dce969e5e582f3f6a7150a
JohnnyYours <103307019+JohnnyYours@users.noreply.github.com>
2022-07-01 17:04:15 +0300

    index.js, index.html and style.css added

---
+++ 7_getFormattedWelcomeMessage/js/index.js
@@ -0,0 +1,8 @@
+//Завершите функцию, чтобы она возвращала отформатированное содержимое тега p.
+
+const getFormattedWelcomeMessage = () => {
+	return document.querySelector("#welcome-message").textContent;
+};
+
+// Sample usage - do not modify
+console.log(getFormattedWelcomeMessage());
