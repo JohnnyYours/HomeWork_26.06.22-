985a40f5fb5db83e21407edf8ee609c69edb6eba
JohnnyYours <103307019+JohnnyYours@users.noreply.github.com>
2022-07-01 17:00:29 +0300

    index.js, index.html and style.css added

---
+++ 3_getDivElements/js/index.js
@@ -0,0 +1,24 @@
+/*
+Завершите работу функций. К каждому методу есть комментарий, объясняющий, что он должен вернуть.
+*/
+
+const getDivElements = () => {
+  // TODO: return all the div elements
+  return document.querySelectorAll("div");
+};
+
+const getFooterLinks = () => {
+  // TODO: return all the links that are in the footer-wrapper
+  return document.querySelectorAll("#footer-wrapper a");
+};
+
+const getAboutLinks = () => {
+  // TODO: return all the links that point to the About Us page
+  return document.querySelectorAll("a[href='about.html']");
+};
+
+
+// Sample usage - do not modify
+console.log(getDivElements());
+console.log(getFooterLinks());
+console.log(getAboutLinks());
